c0bb997331ca085ccf19f5ee650bbfcafa3d1941
fox_meller <monicvalentane@gmail.com>
2025-03-11 09:08:06 +0300

    3d iteration

--- precode.sh
+++ precode.sh
@@ -10,23 +10,23 @@
 mkdir -p task/dir1 task/dir2 task/dir3/dir4
 
 # изменяем текущую директорию на task
-cd /task
+cd task
 
 # создаём пустой файл task/dir2/empty
-touch /dir2/empty
+touch dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 
 # #!/bin/bash
 # echo "$1, привет!"
 echo '!/bin/bash
- echo "$1, привет!' > touch /dir2/hello.sh 
+ echo "$1, привет!' > touch dir2/hello.sh 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
-chmod 764 /dir2/hello.sh 
+chmod 764 dir2/hello.sh 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-ls /dir2 > /dir2/list.txt
+ls dir2 > dir2/list.txt
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r /dir2 /dir3/dir4
+cp -r dir2 dir3/dir4
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
 find . -name "*.txt" > dir1/summary.txt
